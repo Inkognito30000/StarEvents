b6706126e96d90f929a5b830bb5414bc74e84db3
Inkognito30000 <andreymarkov435@yandex.ru>
2024-11-24 23:15:01 +0300

    радиобаттоны

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
 const card = Array.from(document.querySelectorAll('.size-wrapper__card'));
 
-card.forEach((item) => {
+/* card.forEach((item) => {
     item.addEventListener('click', () => {
         item.childNodes[0].classList.add('active')
     })
-})
+}) */
 
 /* console.log(card[2].childNodes[0].textContent); */
+
+/* item.childNodes[0].classList.add('active') */
+
